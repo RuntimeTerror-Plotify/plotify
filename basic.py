f345a81fffe431053c95baf020d5838df4f7eb75
shrushti22 <shrushtivasaniya@gmail.com>
2021-03-26 13:29:32 +0530

    statictical description part done

--- basic.py
+++ basic.py
@@ -16,7 +16,85 @@ def main():
         colName.append(x[0])
         dtypes.append(str(x[1]))
 
-    output = {"shape": df.shape, "col": colName, "dtype": dtypes}
+    count = []
+    for x in df.count():
+        count.append(x)
+
+    categorical = []
+    numerical = []
+    for i in range(df.shape[1]):
+        if dtypes[i] != "object":
+            numerical.append(colName[i])
+        else:
+            categorical.append(colName[i])
+
+    mean = []
+    for x in df.mean():
+        mean.append(x)
+
+    median = []
+    for x in df.median():
+        median.append(x)
+
+    minimum = []
+    for x in df.min():
+        if type(x) != str:
+            minimum.append(x.item())
+
+    maximum = []
+    for x in df.max():
+        if type(x) != str:
+            maximum.append(x.item())
+
+    std = []
+    for x in df.std():
+        std.append(x)
+
+    df.quantile(q=0.25)
+    quant25 = []
+    for x in df.quantile(q=0.25):
+        quant25.append(x)
+
+    quant50 = []
+    for x in df.quantile(q=0.5):
+        quant50.append(x)
+
+    quant75 = []
+    for x in df.quantile(q=0.75):
+        quant75.append(x)
+
+    cat = df.describe(include=[object]).values
+    unique = []
+    for x in cat[1, :]:
+        unique.append(x)
+
+    top = []
+    for x in cat[2, :]:
+        top.append(x)
+
+    freq = []
+    for x in cat[3, :]:
+        freq.append(x.item())
+
+    output = {
+        "shape": df.shape,
+        "columns": colName,
+        "dtype": dtypes,
+        "count": count,
+        "numerical": numerical,
+        "mean": mean,
+        "median": median,
+        "minimum": minimum,
+        "maximum": maximum,
+        "std": std,
+        "quant25": quant25,
+        "quant50": quant50,
+        "quant75": quant75,
+        "categorical": categorical,
+        "unique": unique,
+        "top": top,
+        "freq": freq,
+    }
     output = json.dumps(output)
 
     print(output)
