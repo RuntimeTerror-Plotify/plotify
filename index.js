6a94cde701890d71f2b3deb97d2f2deb0c743977
vrutikrabadia <57054296+vrutikrabadia@users.noreply.github.com>
2021-04-11 15:13:19 +0530

    automatically delete old files

--- index.js
+++ index.js
@@ -81,21 +81,38 @@ function handleFiles() {
   }
 }
 
+const removeDir = function(path) {
+  if (fs.existsSync(path)) {
+    const files = fs.readdirSync(path)
+
+    if (files.length > 0) {
+      files.forEach(function(filename) {
+        if (fs.statSync(path + "/" + filename).isDirectory()) {
+          removeDir(path + "/" + filename)
+        } else {
+          fs.unlinkSync(path + "/" + filename)
+        }
+      })
+      fs.rmdirSync(path)
+    } else {
+      fs.rmdirSync(path)
+    }
+  } else {
+    console.log("Directory path not found.")
+  }
+}
+
 app.get("/", function (req, res) {
   console.log(fs.existsSync(folderPath))
-  if (!fs.existsSync(folderPath)) {
-    console.log("no")
-    fs.mkdirSync(folderPath,function(err){
-      if(err){
-        console.log(err);
-      }
-    })
-    res.render("home_page");
+  if (fs.existsSync(folderPath)) {
+    removeDir(folderPath);   
   }
-  else{
-    res.render("home_page");
-  }
-  
+  fs.mkdirSync(folderPath,function(err){
+    if(err){
+      console.log(err);
+    }
+  })
+  res.render("home_page");
 });
 
 app.get("/tutorial",function(req,res){
