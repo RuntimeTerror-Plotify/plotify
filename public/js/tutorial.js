c108a68f4fcaa79e970b94b8acdff577cb8a5e04
vrutikrabadia <57054296+vrutikrabadia@users.noreply.github.com>
2021-04-17 18:03:13 +0530

    side bar functioning in tutorial

--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -1,10 +1,18 @@
 
     $("." + String(currentPage)).css("display", "block");
+
     $('.page-forward').click(function () {
         $("." + String(parseInt(this.id))).css("display", "none");
         $("." + String(parseInt(this.id) + 1)).css("display", "block");
     });
+
     $('.page-backward').click(function () {
         $("." + String(parseInt(this.id))).css("display", "none");
         $("." + String(parseInt(this.id) - 1)).css("display", "block");
     });
+
+
+    $(".pannel_nav").click(function() {
+        $(".tutorials").css("display","none");
+        $("." + String(parseInt(this.id))).css("display", "block");
+    })
